f5f9cfe6862d0cbc032030f5a2fd94e0d1ef4b0c
fikrimarzuki <fikri.marzuki1993@gmail.com>
2024-10-13 13:27:30 +0700

    fix some daycss

--- 100css/21-30/day29/script.js
+++ 100css/21-30/day29/script.js
@@ -1,12 +1,23 @@
-$('.searchfield').keyup(function() {
-	var $suggestions = $('.suggestions');
-	var text = $(this).val();
-	if(text.length){
-		$suggestions.html('').addClass('active');
-		$suggestions.append( $('<li />', {html: '<b>' + text + '</b>'}) );
-		$suggestions.append( $('<li />', {html: 'Veritatis et <b>' + text + '</b>'}) );
-		$suggestions.append( $('<li />', {html: 'ut aliquid ex <b>' + text + '</b> vero eos'}) );
+const searchField = document.querySelector(".searchfield")
+document.addEventListener("keyup", e => {
+	const suggestions = document.querySelector(".suggestions");
+	let text = e.target.value;
+	if (text.length) {
+		suggestions.innerHTML = "";
+		suggestions.classList.add("active");
+		suggestions.innerHTML += `
+			<li>
+				<b>${text}</b>
+			</li>
+			<li>
+				Ver todos los resultados de <b>${text}</b>
+			</li>
+			<li>
+				ut aliquid ex <b>${text}</b> vero eos
+			</li>
+		`
 	} else {
-		$suggestions.html('').removeClass('active');
+		suggestions.innerHTML = "";
+		suggestions.classList.remove("active");
 	}
-});
+})
